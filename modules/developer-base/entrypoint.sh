8c22489f70a821914aabffe7d71b5f6399084db2
Eric Sauer <esauer@redhat.com>
2024-04-30 17:12:18 +0000

    Move image overrides into their own files; add cekit module for adding cekit to an image; make an attmpe to make rootless podman work

--- modules/developer-base/entrypoint.sh
+++ modules/developer-base/entrypoint.sh
@@ -5,10 +5,8 @@ if [ ! -d "${HOME}" ]; then
   mkdir -p "${HOME}"
 fi
 
-# Setup $PS1 for a consistent and reasonable prompt
-if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
-  echo "PS1='[\u@\h \W]\$ '" > "${HOME}"/.bashrc
-fi
+mkdir -p ${HOME}/.config/containers
+(echo '[storage]';echo 'driver = "vfs"') > ${HOME}/.config/containers/storage.conf
 
 # Add current (arbitrary) user to /etc/passwd and /etc/group
 if ! whoami &> /dev/null; then
@@ -17,5 +15,21 @@ if ! whoami &> /dev/null; then
     echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
   fi
 fi
+USER=$(whoami)
+START_ID=$(( $(id -u)+1 ))
+echo "${USER}:${START_ID}:2147483646" > /etc/subuid
+echo "${USER}:${START_ID}:2147483646" > /etc/subgid
+
+# Setup $PS1 for a consistent and reasonable prompt
+if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
+  echo "PS1='[\u@\h \W]\$ '" > "${HOME}"/.bashrc
+fi
+
+if [ ! -f ${HOME}/.zshrc ]
+then
+  (echo "HISTFILE=${HOME}/.zsh_history"; echo "HISTSIZE=1000"; echo "SAVEHIST=1000") > ${HOME}/.zshrc
+fi
+
+podman login -u $(oc whoami) -p $(oc whoami -t)  image-registry.openshift-image-registry.svc:5000
 
 exec "$@"
--- modules/developer-base/setup-entrypoint.sh
+++ modules/developer-base/setup-entrypoint.sh
@@ -1,9 +1,20 @@
+# Copy entrypoint
 cp /tmp/artifacts/entrypoint.sh /
-# add user and configure it
-useradd -u 10001 -G wheel,root -d /home/user --shell /bin/bash -m user
+chown 0:0 /entrypoint.sh
+
 # Setup $PS1 for a consistent and reasonable prompt
 echo "export PS1='\W \`git branch --show-current 2>/dev/null | sed -r -e \"s@^(.+)@\(\1\) @\"\`$ '" >> /home/user/.bashrc
 # Set permissions on /etc/passwd and /home to allow arbitrary users to write
 chgrp -R 0 /home
-chmod -R g=u /etc/passwd /etc/group /home
-chmod +x /entrypoint.sh
+
+# Setup for root-less podman
+mkdir -p "${HOME}"/.config/containers;
+setcap cap_setuid+ep /usr/bin/newuidmap;
+setcap cap_setgid+ep /usr/bin/newgidmap;
+touch /etc/subgid /etc/subuid;
+chmod +x /entrypoint.sh;
+chmod -R g=u /etc/passwd /etc/group /etc/subuid /etc/subgid /home ${WORK_DIR}
+
+# Setup for dynamic oc & kubectl
+ln -s /projects/bin/oc /usr/local/bin/oc;
+ln -s /projects/bin/kubectl /usr/local/bin/kubectl;
