97a9c6b0141e05d9d705b0ed9113ffa71106081d
cgruver <cgruver@clg.lab>
2024-09-20 16:24:38 +0000

    fix uid/gid for helm and kustomize

--- modules/openshift-tools/helm.sh
+++ modules/openshift-tools/helm.sh
@@ -4,5 +4,6 @@ arch=linux-amd64
 curl -sLO "https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/helm/${version}/helm-${arch}.tar.gz"
 tar xzf ./helm-${arch}.tar.gz
 mv ./helm-${arch} /usr/bin/helm
+chown root:root /usr/bin/helm
 helm version
 echo "😎😎😎😎😎"
--- modules/openshift-tools/kustomize.sh
+++ modules/openshift-tools/kustomize.sh
@@ -4,5 +4,6 @@ arch=linux_amd64
 curl -sLO "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2F${version}/kustomize_${version}_${arch}.tar.gz"
 tar xzf ./kustomize_${version}_${arch}.tar.gz
 mv ./kustomize /usr/bin/
+chown root:root /usr/bin/kustomize
 kustomize version
 echo "😎😎😎😎😎"
