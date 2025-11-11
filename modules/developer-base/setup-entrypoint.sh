667b13ae3b6916980f65502dfeee8d07e0fd345e
Eric Sauer <etsauer@gmail.com>
2025-11-11 14:25:39 -0500

    Disable root-less podman setup commands
    Comment out setup commands for root-less podman.

--- modules/developer-base/setup-entrypoint.sh
+++ modules/developer-base/setup-entrypoint.sh
@@ -8,13 +8,13 @@ echo "export PS1='\W \`git branch --show-current 2>/dev/null | sed -r -e \"s@^(.
 chgrp -R 0 /home /etc/passwd /etc/group /etc/subuid /etc/subgid /home ${WORK_DIR}
 
 # Setup for root-less podman
-mkdir -p "${HOME}"/.config/containers;
-mkdir -p "${HOME}/.cache"
-setcap cap_setuid+ep /usr/bin/newuidmap;
-setcap cap_setgid+ep /usr/bin/newgidmap;
-touch /etc/subgid /etc/subuid;
-chmod +x /entrypoint.sh;
-chmod -R g=u /etc/passwd /etc/group /etc/subuid /etc/subgid /home ${WORK_DIR}
+#mkdir -p "${HOME}"/.config/containers;
+#mkdir -p "${HOME}/.cache"
+#setcap cap_setuid+ep /usr/bin/newuidmap;
+#setcap cap_setgid+ep /usr/bin/newgidmap;
+#touch /etc/subgid /etc/subuid;
+#chmod +x /entrypoint.sh;
+#chmod -R g=u /etc/passwd /etc/group /etc/subuid /etc/subgid /home ${WORK_DIR}
 
 # Setup for dynamic oc & kubectl
 ln -s /projects/bin/oc /usr/local/bin/oc;
