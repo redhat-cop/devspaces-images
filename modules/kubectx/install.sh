a92accbfd2a461895dabba2593f7d89635ef0e3a
Eric Sauer <esauer@redhat.com>
2025-12-16 21:34:51 +0000

    make sure kubectx and kubens are executable

--- modules/kubectx/install.sh
+++ modules/kubectx/install.sh
@@ -3,5 +3,8 @@
 curl -sSL -o /opt/kubectx https://raw.githubusercontent.com/ahmetb/kubectx/refs/heads/master/kubectx
 curl -sSL -o /opt/kubectx https://raw.githubusercontent.com/ahmetb/kubectx/refs/heads/master/kubens
 
-ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+chmod +x /opt/kubectx
+chmod +x /opt/kubens
+
+ln -s /opt/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx /usr/local/bin/kubens
