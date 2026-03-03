807257b0dcdc4c05c4439841cc80099c33d6d8f7
SamPlaysKeys <sfleming@redhat.com>
2026-03-03 16:34:42 -0500

    Add destination paths for downloads

--- modules/vault/install.sh
+++ modules/vault/install.sh
@@ -7,12 +7,12 @@ mkdir -p ~/tmp
 PRODUCT="vault"
 VERSION="1.21.3"
 
-wget https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_linux_amd64.zip
-wget https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_SHA256SUMS
-wget https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_SHA256SUMS.sig
+wget -O ~/tmp/${PRODUCT}_${VERSION}_linux_amd64.zip https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_linux_amd64.zip
+wget -O ~/tmp/${PRODUCT}_${VERSION}_SHA256SUMS https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_SHA256SUMS
+wget -O ~/tmp/${PRODUCT}_${VERSION}_SHA256SUMS.sig https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_SHA256SUMS.sig
 wget -qO- https://www.hashicorp.com/.well-known/pgp-key.txt | gpg --import
-gpg --verify ${PRODUCT}_${VERSION}_SHA256SUMS.sig ${PRODUCT}_${VERSION}_SHA256SUMS
-grep ${PRODUCT}_${VERSION}_linux_amd64.zip ${PRODUCT}_${VERSION}_SHA256SUMS | sha256sum -c 
+gpg --verify ~/tmp/${PRODUCT}_${VERSION}_SHA256SUMS.sig ~/tmp/${PRODUCT}_${VERSION}_SHA256SUMS
+grep ~/tmp/${PRODUCT}_${VERSION}_linux_amd64.zip ~/tmp/${PRODUCT}_${VERSION}_SHA256SUMS | sha256sum -c 
 unzip ~/tmp/${PRODUCT}_${VERSION}_linux_amd64.zip -d ~/tmp
 mv ~/tmp/${PRODUCT} /usr/local/bin/${PRODUCT}
 chmod +x /usr/local/bin/${PRODUCT}
